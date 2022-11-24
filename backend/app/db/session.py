6d79a3cfc78f16f11971a8d5a6560ff72b26ec3c
jermainewright <verifiedbosco@gmail.com>
2022-11-24 17:04:00 +0100

    Database session & base models

---
+++ backend/app/db/session.py
@@ -0,0 +1,44 @@
+"""
+Async SQLAlchemy engine and session factory.
+Uses asyncpg driver for high-performance async PostgreSQL access.
+"""
+
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.core.config import settings
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=not settings.is_production,
+    pool_size=10,
+    max_overflow=20,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+)
+
+AsyncSessionFactory = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False,
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    """FastAPI dependency that yields a database session."""
+    async with AsyncSessionFactory() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
